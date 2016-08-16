0cf8f5fed1589079515392ab1ede786ea923cee4
Andrew Haller <stairwaytowonderland@gmail.com>
2016-08-15 22:01:35 -0400

    Toolbar now hidden initially.

--- generator_editable.php
+++ generator_editable.php
@@ -13,7 +13,7 @@
       <link rel="stylesheet" href="<?php echo $theme_url ?>/css/generator.css" />
     </head>
     <body>
-      <div id="toolbar" class="container-fluid shadow-bottom">
+      <div id="toolbar" class="container-fluid shadow-bottom hidden">
         <div class="row">
           <div class="col-xs-12">
             <form id="editorOptions" class="form-inline">
